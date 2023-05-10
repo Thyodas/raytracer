f37dcf9d42e0bee59ed12f0707b180818aba4029
iMeaNz <mehdy.morvan@epitech.eu>
2023-05-10 15:57:27 +0200

    FEAT: Added scenes

--- src/main.cpp
+++ src/main.cpp
@@ -12,6 +12,7 @@
 #include "Core.hpp"
 #include "../shared/math/Vectors/Vec3.hpp"
 #include "../shared/math/Vectors/Vec2.hpp"
+#include "../shared/math/Matrix/MatrixTransformation.hpp"
 #include "../shared/math/Matrix/Matrix44.hpp"
 #include "Parser.hpp"
 #include "Parser/ObjParser.hpp"
@@ -19,13 +20,127 @@
 #include <vector>
 #include <memory>
 
+inline float randomFloat()
+{
+    return rand() / (RAND_MAX + 1.0);
+}
+
+inline double randomFloat(double min, double max)
+{
+    return min + (max - min) * randomFloat();
+}
+
+void randomScene(raytracer::Core &core)
+{
+    srand(time(NULL));
+    for (int i = -2; i < 2; ++i) {
+        for (int j = -2; j < 2; ++j) {
+            float randomMat = randomFloat();
+            Vec3f center(i + randomFloat(-10, 10), 3, j +  randomFloat(-10, 10));
+            Matrix44f xFormSphere;
+            xFormSphere[3][0] = center.x;
+            xFormSphere[3][1] = center.y;
+            xFormSphere[3][2] = center.z;
+            if (randomMat < 0.8) {
+                Vec3f randomColor(randomFloat(), randomFloat(), randomFloat());
+                primitive::Sphere *sph = new primitive::Sphere(xFormSphere, randomFloat(0.5, 2));
+                sph->albedo = randomColor;
+                sph->kd = randomFloat(0.05, 0.5);
+                sph->ks = randomFloat(0.05, 0.5);
+                sph->materialType = primitive::DIFFUSE_AND_GLOSSY;
+                sph->specularExponent = randomFloat(2, 200);
+                core.addObject(std::shared_ptr<primitive::Object>(sph));
+            } else {
+                primitive::Sphere *sph = new primitive::Sphere(xFormSphere, randomFloat(0.5, 2));
+                sph->refractionCoefficient = randomFloat(0.5, 2);
+                sph->materialType = primitive::REFLECTION_AND_REFRACTION;
+                core.addObject(std::shared_ptr<primitive::Object>(sph));
+            }
+        }
+    }
+}
+
+void multipleSphereScene(raytracer::Core &core)
+{
+    float w[5] = {0.04, 0.08, 0.1, 0.2, 0.4};
+    float z[5] = {5, 8, 10, 7, 6};
+    float radius = 0.9;
+    Vec3f c[5] = {Vec3f(0.2, 0, 0), Vec3f(0, 0.2, 0),Vec3f(0,0,0.2), Vec3f(0.2,0.2,0), Vec3f(0.2, 0.2,0.2)};
+    for (int i = -4, n = 2, k = 0; i <= 4; i+= 2, n *= 2, k++) {
+        Matrix44f xformSphere;
+        xformSphere[3][0] = i;
+        xformSphere[3][1] = 2;
+        xformSphere[3][2] = 5;
+        // if (i == 2) {
+        //     xformSphere[3][0] = 0;
+        //     xformSphere[3][2] = 7;
+        //     xformSphere[3][1] = 3.8;
+        //     radius = 0.5;
+        // }
+        primitive::Sphere *sph = new primitive::Sphere(xformSphere, radius);
+        if (i == -4) {
+            //sph->materialType = primitive::REFLECTION_AND_REFRACTION;
+            sph->txtType = primitive::CHECKER;
+        }
+        sph->specularExponent = n;
+        sph->ks = w[k];
+        sph->albedo = c[k];
+        core.addObject(std::shared_ptr<primitive::Object>(sph));
+        radius = 0.9;
+    }
+}
+
+void oneDiffuseSphereOneRefractionSphere(raytracer::Core &core)
+{
+    Matrix44f transformSph1;
+    math::translate(transformSph1, Vec3f(-1, 0, -12));
+    primitive::Sphere *sph1 = new primitive::Sphere(transformSph1, 2);
+    sph1->materialType = primitive::DIFFUSE_AND_GLOSSY;
+    sph1->albedo = Vec3f(0.6, 0.0, 0.0);
+    Matrix44f transformSph2;
+    math::translate(transformSph2, Vec3f(0.9, -0.5, -8));
+    primitive::Sphere *sph2 = new primitive::Sphere(transformSph2, 1.5);
+    sph2->refractionCoefficient = 1.5;
+    sph2->kd = 0.5;
+    sph2->ks = 0.5;
+    sph2->materialType = primitive::REFLECTION_AND_REFRACTION;
+
+    core.addObject(std::shared_ptr<primitive::Object>(sph1));
+    core.addObject(std::shared_ptr<primitive::Object>(sph2));
+}
+
+void teapotScene(raytracer::Core &core)
+{
+    Parser::ObjParserData::transformationsOptions opt = {
+        .pos = Vec3f(0, 3, 12),
+        .scaleFactorX = 0.05,
+        .scaleFactorY = 0.05,
+        .scaleFactorZ = 0.05,
+        .rotateXAxis = 80,
+        // .rotateYAxis = -90,
+        // .rotateZAxis = -90,
+        //.rotateZAxis = 45,
+        .color = Vec3f(0.7, 0, 0),
+    };
+    Parser::parseObj(core, opt, "./teapot.obj", false);
+    Matrix44f transformSph1;
+    math::translate(transformSph1, Vec3f(2, 2, 5));
+    primitive::Sphere *sph1 = new primitive::Sphere(transformSph1, 2);
+    sph1->materialType = primitive::DIFFUSE_AND_GLOSSY;
+    sph1->albedo = Vec3f(0.6, 0.0, 0.0);
+    core.addObject(std::shared_ptr<primitive::Object>(sph1));
+}
+
 int main(int argc, char **argv)
 {
     //Setup Core
-    Matrix44f cameraToWorld = Matrix44f(1, 0, 0, 0,
-                                        0, 1, 0, 0,
-                                        0, 0, 1, 0,
-                                        0, 3, 10, 1);
+    // Matrix44f cameraToWorld = Matrix44f(1, 0, 0, 0,
+    //                                     0, 1, 0, 0,
+    //                                     0, 0, 1, 0,
+    //                                     0, 3, 12, 1);
+    Matrix44f cameraToWorld;
+    //math::rotateAroundXAxis(cameraToWorld, math::deg2Rad(45));
+    math::translate(cameraToWorld, Vec3f(0, 5, 8));
     raytracer::Core core(cameraToWorld);
     core.setFov(90);
 
@@ -44,24 +159,18 @@ int main(int argc, char **argv)
     Parser::parseObj(core, opt, "./plane.obj", true);
 
     //Setup lights
-    Matrix44f l2w(11.146836, -5.781569, -0.0605886, 0,
-                 -1.902827, -3.543982, -11.895445, 0,
-                 5.459804, 10.568624, -4.02205, 0,
-                 0, 1, 0, 1);
+    Matrix44f l2w;
+    math::translate(l2w, Vec3f(0, 1, 0));
+    math::rotateAroundYAxis(l2w, math::deg2Rad(45));
+    math::rotateAroundZAxis(l2w, math::deg2Rad(45));
+    // Matrix44f l2w(11.146836, -5.781569, -0.0605886, 0,
+    //              -1.902827, -3.543982, -11.895445, 0,
+    //              5.459804, 10.568624, -4.02205, 0,
+    //              0, 1, 0, 1);
     core.addLight(std::shared_ptr<physics::Light>(new physics::DistantLight(l2w, 1, 1)));
-    float w[5] = {0.04, 0.08, 0.1, 0.15, 0.2};
-    Vec3f c[5] = {Vec3f(0.2, 0, 0), Vec3f(0, 0.2, 0),Vec3f(0,0,0.2), Vec3f(0.2,0.2,0), Vec3f(0.2, 0.2,0.2)};
-    for (int i = -4, n = 2, k = 0; i <= 4; i+= 2, n *= 5, k++) {
-        Matrix44f xformSphere;
-        xformSphere[3][0] = i;
-        xformSphere[3][1] = 2;
-        xformSphere[3][2] = 5;
-         primitive::Sphere *sph = new primitive::Sphere(xformSphere, 0.9);
-        sph->specularExponent = n;
-        sph->ks = w[k];
-        sph->albedo = c[k];
-        core.addObject(std::shared_ptr<primitive::Object>(sph));
-    }
+    //randomScene(core);
+    //multipleSphereScene(core);
+    teapotScene(core);
 
 
     core.render();
